a67c429b7c2f275a7756e8dea73af11881394892
Hassan Malik <hbmalik88@gmail.com>
2018-07-28 16:40:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return object;
 }
 function deleteFromObjectByKey (object, key) {
-  
+  delete object[key];
+  return object;
 }
